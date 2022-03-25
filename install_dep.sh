9bfffa3bf65cfe1e3107904a5f91c487a22d9579
Terrioux <32219776+Terrioux@users.noreply.github.com>
2022-03-25 12:44:49 +0100

    Delete install_dep.sh

--- install_dep.sh
+++
@@ -1,7 +0,0 @@
-mvn install:install-file \
-  -DgroupId=org.chocosolver \
-  -DartifactId=choco-solver \
-  -Dpackaging=jar \
-  -Dversion=4.10.7 \
-  -Dfile=libs/choco-parsers-4.10.7-SNAPSHOT-jar-with-dependencies.jar \
-  -DgeneratePom=true
