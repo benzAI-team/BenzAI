f8aac4d12cd4cda87b4c86ea9ac4336604cbb803
Adrien VARET <adrien.varet@etu.univ-amu.fr>
2022-03-28 11:13:56 +0200

    modifying symmetries name (for generator)

--- src/main/java/view/generator/boxes/HBoxSymmetriesCriterion.java
+++ src/main/java/view/generator/boxes/HBoxSymmetriesCriterion.java
@@ -41,20 +41,20 @@ public class HBoxSymmetriesCriterion extends HBoxCriterion {
 
 		symmetriesChoiceBox = new ChoiceBox<>();
 
-		symmetriesChoiceBox.getItems().add("face-mirror");
-		symmetriesChoiceBox.getItems().add("(face)-60-rotation");
-		symmetriesChoiceBox.getItems().add("face-120-rotation");
-		symmetriesChoiceBox.getItems().add("Rotation of 180°");
-		symmetriesChoiceBox.getItems().add("edge-mirror");
-		symmetriesChoiceBox.getItems().add("vertex-120-rotation");
-		symmetriesChoiceBox.getItems().add("edge-180-rotation");
+		symmetriesChoiceBox.getItems().add("C_2v(a) \"face-mirror\"");
+		symmetriesChoiceBox.getItems().add("C_6h \"(face)-60-rotation\"");
+		symmetriesChoiceBox.getItems().add("C_3h(i) \"face-120-rotation\"");
+		symmetriesChoiceBox.getItems().add("C_2h(i) \"vertex_180-rotation\"");
+		symmetriesChoiceBox.getItems().add("C_2v(b) \"edge-mirror\"");
+		symmetriesChoiceBox.getItems().add("C_3h(ii) \"vertex-120-rotation\"");
+		symmetriesChoiceBox.getItems().add("C_2h(ii) \"edge-180-rotation\"");
 
-		symmetriesChoiceBox.getItems().add("(vertex)-60-rotation+(edge)-mirror");
-		symmetriesChoiceBox.getItems().add("vertex-120-rotation+(edge)-mirror");
-		symmetriesChoiceBox.getItems().add("face-120-rotation+face-mirror");
-		symmetriesChoiceBox.getItems().add("face-120-rotation+edge-mirror");
-		symmetriesChoiceBox.getItems().add("edge-180-rotation+edge-mirror");
-		symmetriesChoiceBox.getItems().add("face-180-rotation+edge-mirror");
+		symmetriesChoiceBox.getItems().add("D_6h \"(vertex)-60-rotation+(edge)-mirror\"");
+		symmetriesChoiceBox.getItems().add("D_3h(ii) \"vertex-120-rotation+(edge)-mirror\"");
+		symmetriesChoiceBox.getItems().add("D_3h(ia) \"face-120-rotation+face-mirror\"");
+		symmetriesChoiceBox.getItems().add("D_3h(ib) \"face-120-rotation+edge-mirror\"");
+		symmetriesChoiceBox.getItems().add("D_2h(ii) \"edge-180-rotation+edge-mirror\"");
+		symmetriesChoiceBox.getItems().add("D_2h(i) \"face-180-rotation+edge-mirror\"");
 
 		symmetriesChoiceBox.getSelectionModel().selectFirst();
 
@@ -71,43 +71,43 @@ public class HBoxSymmetriesCriterion extends HBoxCriterion {
 
 		ArrayList<GeneratorCriterion> criterions = new ArrayList<>();
 
-		if (symmetriesChoiceBox.getValue().equals("face-mirror"))
+		if (symmetriesChoiceBox.getValue().equals("C_2v(a) \"face-mirror\""))
 			criterions.add(new GeneratorCriterion(Subject.SYMM_MIRROR, Operator.NONE, ""));
 
-		else if (symmetriesChoiceBox.getValue().equals("(face)-60-rotation"))
+		else if (symmetriesChoiceBox.getValue().equals("C_6h \"(face)-60-rotation\""))
 			criterions.add(new GeneratorCriterion(Subject.SYMM_ROT_60, Operator.NONE, ""));
 
-		else if (symmetriesChoiceBox.getValue().equals("face-120-rotation"))
+		else if (symmetriesChoiceBox.getValue().equals("C_3h(i) \"face-120-rotation\""))
 			criterions.add(new GeneratorCriterion(Subject.SYMM_ROT_120, Operator.NONE, ""));
 
-		else if (symmetriesChoiceBox.getValue().equals("vertex-180-rotation"))
+		else if (symmetriesChoiceBox.getValue().equals("C_2h(i) \"vertex_180-rotation\""))
 			criterions.add(new GeneratorCriterion(Subject.SYMM_ROT_180, Operator.NONE, ""));
 
-		else if (symmetriesChoiceBox.getValue().equals("edge-mirror"))
+		else if (symmetriesChoiceBox.getValue().equals("C_2v(b) \"edge-mirror\""))
 			criterions.add(new GeneratorCriterion(Subject.SYMM_VERTICAL, Operator.NONE, ""));
 
-		else if (symmetriesChoiceBox.getValue().equals("vertex-120-rotation"))
+		else if (symmetriesChoiceBox.getValue().equals("C_3h(ii) \"vertex-120-rotation\""))
 			criterions.add(new GeneratorCriterion(Subject.SYMM_ROT_120_V, Operator.NONE, ""));
 
-		else if (symmetriesChoiceBox.getValue().equals("edge-180-rotation"))
+		else if (symmetriesChoiceBox.getValue().equals("C_2h(ii) \"edge-180-rotation\""))
 			criterions.add(new GeneratorCriterion(Subject.SYMM_ROT_180_E, Operator.NONE, ""));
 
-		else if (symmetriesChoiceBox.getValue().equals("(vertex)-60-rotation+(edge)-mirror"))
+		else if (symmetriesChoiceBox.getValue().equals("D_6h \"(vertex)-60-rotation+(edge)-mirror\""))
 			criterions.add(new GeneratorCriterion(Subject.SYMM_ROT_60, Operator.NONE, ""));
 
-		else if (symmetriesChoiceBox.getValue().equals("vertex-120-rotation+(edge)-mirror"))
+		else if (symmetriesChoiceBox.getValue().equals("D_3h(ii) \"vertex-120-rotation+(edge)-mirror\""))
 			criterions.add(new GeneratorCriterion(Subject.ROT_120_VERTEX_MIRROR, Operator.NONE, ""));
 
-		else if (symmetriesChoiceBox.getValue().equals("face-120-rotation+face-mirror"))
+		else if (symmetriesChoiceBox.getValue().equals("D_3h(ia) \"face-120-rotation+face-mirror\""))
 			criterions.add(new GeneratorCriterion(Subject.ROT_120_MIRROR_H, Operator.NONE, ""));
 
-		else if (symmetriesChoiceBox.getValue().equals("face-120-rotation+edge-mirror"))
+		else if (symmetriesChoiceBox.getValue().equals("D_3h(ib) \"face-120-rotation+edge-mirror\""))
 			criterions.add(new GeneratorCriterion(Subject.ROT_120_MIRROR_E, Operator.NONE, ""));
 
-		else if (symmetriesChoiceBox.getValue().equals("edge-180-rotation+edge-mirror"))
+		else if (symmetriesChoiceBox.getValue().equals("D_2h(ii) \"edge-180-rotation+edge-mirror\""))
 			criterions.add(new GeneratorCriterion(Subject.ROT_180_EDGE_MIRROR, Operator.NONE, ""));
 
-		else if (symmetriesChoiceBox.getValue().equals("face-180-rotation+edge-mirror"))
+		else if (symmetriesChoiceBox.getValue().equals("D_2h(i) \"face-180-rotation+edge-mirror\""))
 			criterions.add(new GeneratorCriterion(Subject.ROT_180_MIRROR, Operator.NONE, ""));
 
 		return criterions;
