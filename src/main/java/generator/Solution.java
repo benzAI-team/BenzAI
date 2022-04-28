842ce71fd9a349598db79c9e7fc4aa38bfb76f27
Adrien <adrien.varet@lis-lab.fr>
2022-04-28 11:16:24 +0200

    classe solution

--- src/main/java/generator/Solution.java
+++ src/main/java/generator/Solution.java
@@ -3,82 +3,123 @@ package generator;
 import java.util.ArrayList;
 
 import molecules.Node;
-import utils.Utils;
+
 
 public class Solution {
-
-	private int nbHexagons;
 
 	private int[] vertices;
 	private int[][] coordsMatrixCoronenoid;
 	private Node[] coronenoidNodes;
-	private int[][] neighbors;
 
-	public Solution(Node[] coronenoidNodes, int[][] coordsMatrixCoronenoid, int nbHexagons, int[] vertices) {
+	public Solution(Node[] coronenoidNodes, int[][] coordsMatrixCoronenoid, int[] vertices) {
 		this.coronenoidNodes = coronenoidNodes;
 		this.coordsMatrixCoronenoid = coordsMatrixCoronenoid;
-		this.nbHexagons = nbHexagons;
 		this.vertices = vertices;
-		buildNeighbors();
-		System.out.print("");
 	}
 
-	private void buildNeighbors() {
-
+	
+	
+	public int [] getVertices() {
+		return vertices;
+	}
+	
+	public int getVertex(int index) {
+		return vertices[index];
+	}
+	
+	public Node [] getCoronenoidNodes() {
+		return coronenoidNodes;
+	}
+	
+	public Node getCoronenoidNode(int index) {
+		return coronenoidNodes[index];
+	}
+	
+	public int [] rotation180() {
+		
 		int diameter = coordsMatrixCoronenoid.length;
-		neighbors = new int[vertices.length][6];
-
-		for (int i = 0; i < neighbors.length; i++)
-			for (int j = 0; j < 6; j++)
-				neighbors[i][j] = -1;
-
-		for (int i = 0; i < vertices.length; i++) {
-
-			int vertex = vertices[i];
-			Node node = coronenoidNodes[vertex];
-			int x = node.getX();
-			int y = node.getY();
-
-			if (x > 0) { // HAUT-DROIT
-				int neighbor = coordsMatrixCoronenoid[x - 1][y];
-				if (Utils.contains(vertices, neighbor))
-					neighbors[i][0] = neighbor;
-			}
-
-			if (y + 1 < diameter) { // DROIT
-				int neighbor = coordsMatrixCoronenoid[x][y + 1];
-				if (Utils.contains(vertices, neighbor))
-					neighbors[i][1] = neighbor;
-			}
-
-			if (x + 1 < diameter && y + 1 < diameter) { // BAS-DROIT
-				int neighbor = coordsMatrixCoronenoid[x + 1][y + 1];
-				if (Utils.contains(vertices, neighbor))
-					neighbors[i][2] = neighbor;
-			}
-
-			if (x + 1 < diameter) { // BAS-GAUCHE
-				int neighbor = coordsMatrixCoronenoid[x + 1][y];
-				if (Utils.contains(vertices, neighbor))
-					neighbors[i][3] = neighbor;
-			}
-
-			if (y > 0) { // GAUCHE
-				int neighbor = coordsMatrixCoronenoid[x][y - 1];
-				if (Utils.contains(vertices, neighbor))
-					neighbors[i][4] = neighbor;
-			}
-
-			if (x > 0 && y > 0) { // GAUCHE
-				int neighbor = coordsMatrixCoronenoid[x - 1][y - 1];
-				if (Utils.contains(vertices, neighbor))
-					neighbors[i][5] = neighbor;
-			}
+		int centerIndex = coordsMatrixCoronenoid[(diameter - 1) / 2][(diameter - 1) / 2];
+		Node center = getCoronenoidNode(centerIndex);
+		
+		int xc = center.getX();
+		int yc = center.getX();
+		
+		int [] rotation = new int[vertices.length];
+	
+		for (int i = 0 ; i < vertices.length ; i++) {
+			
+			int vertex = getVertex(i);
+			int x1 = getCoronenoidNode(vertex).getX();
+			int y1 = getCoronenoidNode(vertex).getY();
+			
+			int xd = xc - x1;
+			int yd = yc - y1;
+			
+			int x2 = xc + xd;
+			int y2 = yc + yd;
+			
+			int image = coordsMatrixCoronenoid[x2][y2];
+			rotation[i] = image;
 		}
+		
+		return rotation;
 	}
+	
+	public ArrayList<Integer[]> translationsFaceMirror() {
+		
+		int diameter = coordsMatrixCoronenoid.length;
+		
+		ArrayList<Integer[]> translations = new ArrayList<>();
+		
+		int [] rotation = rotation180();
+		
+		for (int shift = -1 * diameter ; shift <= diameter ; shift ++) {
+			
+			Integer[] translation1 = new Integer[vertices.length];
+			Integer [] translation2 = new Integer[vertices.length];
+			
+			boolean embedded1 = true;
+			boolean embedded2 = true;
+			
+			for (int i = 0 ; i < vertices.length ; i++) {
+				
+				int vertexIndex1 = vertices[i];
+				int vertexIndex2 = rotation[i];
+				
+				Node n1 = coronenoidNodes[vertexIndex1];
+				Node n2 = coronenoidNodes[vertexIndex2];
 
-	public ArrayList<Integer[]> rotations() {
-
-		return null;
+				int x1 = n1.getX() + shift;
+				int y1 = n1.getY() + shift;
+				
+				int x2 = n2.getX() + shift;
+				int y2 = n2.getX() + shift;
+				
+				if (!(x1 >= 0 && x1 < diameter && y1 >= 0 && y1 < diameter)) {
+					embedded1 = false;
+					break;
+				}
+				
+				else
+					translation1[i] = coordsMatrixCoronenoid[x1][y1];
+				
+				if (!(x2 >= 0 && x2 < diameter && y2 >= 0 && y2 < diameter)) {
+					embedded2 = false;
+					break;
+				}
+				
+				else
+					translation2[i] = coordsMatrixCoronenoid[x2][y2];
+				
+			}
+			
+			if (embedded1)
+				translations.add(translation1);
+			
+			if (embedded2)
+				translations.add(translation2);
+		}
+		
+		return translations;
 	}
 }
