6eb70b1f797e52bb61cb7215e027ad97f3ad9e6d
Adrien VARET <adrien.varet@etu.univ-amu.fr>
2022-05-11 23:24:20 +0200

    jeudi

--- src/main/java/database/models/IMS2D1A.java
+++ src/main/java/database/models/IMS2D1A.java
@@ -1,5 +1,15 @@
 package database.models;
 
+import molecules.Geometry;
+
 public class IMS2D1A {
 
+	private Long moleculeId;
+	private String name;
+	private int nbHexagons;
+	private int nbCarbons;
+	private int nbHydrogens;
+	private double irregularity;
+
+	private Geometry geometry;
 }
---
+++ src/main/java/molecules/AtomGeometry.java
@@ -0,0 +1,35 @@
+package molecules;
+
+import utils.Triplet;
+
+public class AtomGeometry {
+
+	private String label;
+	private Triplet<Double, Double, Double> geometry;
+
+	public AtomGeometry(String label, double x, double y, double z) {
+		this.label = label;
+		geometry = new Triplet<>(x, y, z);
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public double getX() {
+		return geometry.getX();
+	}
+
+	public double getY() {
+		return geometry.getY();
+	}
+
+	public double getZ() {
+		return geometry.getZ();
+	}
+
+	@Override
+	public String toString() {
+		return label + " : " + geometry.toString();
+	}
+}
