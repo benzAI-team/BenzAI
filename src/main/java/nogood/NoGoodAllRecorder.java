dbc143a02f602c27b4d35075cc55729d1b4c7c8f
nprcovic <nicolas.prcovic@univ-amu.fr>
2023-04-04 18:14:24 +0200

    reste 120edge

---
+++ src/main/java/nogood/NoGoodAllRecorder.java
@@ -0,0 +1,19 @@
+package nogood;
+
+import java.util.ArrayList;
+
+import generator.GeneralModel;
+import generator.Solution;
+
+public class NoGoodAllRecorder extends NoGoodRecorder {
+
+	public NoGoodAllRecorder(GeneralModel model, Solution solution) {
+		super(model, solution);
+	}
+
+	@Override
+	protected ArrayList<ArrayList<Integer>> computeOccurences() {
+		return getSolution().allTranslations();
+	}
+
+}
