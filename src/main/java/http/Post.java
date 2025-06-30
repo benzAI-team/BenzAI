4d01e6a9433907ce3903ba672f9874c750943dae
Terrioux <cyril.terrioux@univ-amu.fr>
2025-06-30 22:58:56 +0200

    Revert change

--- src/main/java/http/Post.java
+++ src/main/java/http/Post.java
@@ -18,8 +18,7 @@ import java.util.regex.Pattern;
 public class Post {
 
 	public static boolean isDatabaseConnected;
-//  private static String databaseServername = "https://benzenoids.lis-lab.fr";
-  private static String databaseServername = "http://localhost:8894/benzenoids";
+  private static String databaseServername = "https://benzenoids.lis-lab.fr";
 
 	@SuppressWarnings("rawtypes")
 	public static List<Map> post(String service, String jsonInputString) throws IOException {
