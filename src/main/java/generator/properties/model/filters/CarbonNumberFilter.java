34d7324bcfa9595cae45d3107952cc14ca9ff4ca
Terrioux <cyril.terrioux@univ-amu.fr>
2025-07-17 16:17:14 +0200

    Fix the filtering based on even/odd carbon number

--- src/main/java/generator/properties/model/filters/CarbonNumberFilter.java
+++ src/main/java/generator/properties/model/filters/CarbonNumberFilter.java
@@ -1,21 +1,28 @@
 package generator.properties.model.filters;
 
-import java.util.ArrayList;
-
-
-import generator.properties.model.ModelPropertySet;
 import benzenoid.Benzenoid;
+import generator.properties.model.ModelPropertySet;
 import generator.properties.model.expression.BinaryNumericalExpression;
 import generator.properties.model.expression.PropertyExpression;
+
+import java.util.ArrayList;
 
 public class CarbonNumberFilter extends Filter {
 
 	@Override
 	public boolean test(Benzenoid molecule, ArrayList<PropertyExpression> propertyExpressionList, ModelPropertySet modelPropertySet) {
 		for(PropertyExpression expression : propertyExpressionList) {
-			int nbCarbons = ((BinaryNumericalExpression)expression).getValue();
-			if(!((BinaryNumericalExpression)expression).test(molecule.getNbCarbons(), ((BinaryNumericalExpression)expression).getOperator(), nbCarbons))
-				return false;
+			if (expression.toString().equals("carbons even")) {
+				return molecule.getNbCarbons() % 2 == 0;
+			} else if  (expression.toString().equals("carbons odd")) {
+				return molecule.getNbCarbons() % 2 != 0;
+			}
+			else {
+				int nbCarbons = ((BinaryNumericalExpression) expression).getValue();
+
+				if (!((BinaryNumericalExpression) expression).test(molecule.getNbCarbons(), ((BinaryNumericalExpression) expression).getOperator(), nbCarbons))
+					return false;
+			}
 		}
 		return true;
 	}
